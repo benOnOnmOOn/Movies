5a50e6b726bcb8e7d7ba184bb574d1801a3d23b6
Benedykt Ziobro <benedykt@backbase.com>
2023-04-27 11:20:34 +0200

    tools : update dependcies

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -17,47 +17,49 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            release {
+                isMinifyEnabled = true
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs = listOf("-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi")
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+
+    dependencies {
+        api(libs.koin.core)
+
+        ksp(libs.moshi.kotlin.codegen)
+
+        implementation(libs.moshi)
+        implementation(libs.converter.moshi)
+        implementation(libs.logging.interceptor)
+        implementation(libs.okhttp)
+        implementation(libs.retrofit)
+        implementation(libs.koin.android)
+        implementation(libs.kotlinx.coroutines.core)
+
+        testRuntimeOnly(libs.junit.engine)
+
+        testImplementation(libs.mockk.dsl)
+        testImplementation(libs.junit)
+        testImplementation(libs.kotlinx.coroutines.test)
+        testImplementation(libs.mockk)
+        testImplementation(libs.mockwebserver)
+
+        androidTestImplementation(libs.androidx.test.ext.junit)
+        androidTestImplementation(libs.androidx.monitor)
+        androidTestImplementation(libs.junit)
+
+        runtimeOnly(libs.kotlinx.coroutines.android)
     }
-    kotlinOptions {
-        jvmTarget = "17"
-        freeCompilerArgs = listOf("-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi")
-    }
-}
-
-dependencies {
-    api(libs.koin.core)
-
-    ksp(libs.moshi.kotlin.codegen)
-
-    implementation(libs.moshi)
-    implementation(libs.converter.moshi)
-    implementation(libs.logging.interceptor)
-    implementation(libs.okhttp)
-    implementation(libs.retrofit)
-    implementation(libs.koin.android)
-    implementation(libs.kotlinx.coroutines.core)
-
-    testRuntimeOnly(libs.junit.engine)
-
-    testImplementation(libs.mockk.dsl)
-    testImplementation(libs.junit)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.mockk)
-    testImplementation(libs.mockwebserver)
-
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.monitor)
-    androidTestImplementation(libs.junit)
-
-    runtimeOnly(libs.kotlinx.coroutines.android)
 }
