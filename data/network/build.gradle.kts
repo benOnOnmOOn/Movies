b6b38fd7ad3f1fa6de124926cb9c76f9e865934d
Benedykt Ziobro <benedykt@backbase.com>
2023-05-08 20:59:16 +0200

    fix : remove duplicated method

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.koin.android)
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.androidx.core)
     implementation(libs.androidx.core.ktx)
 
     testRuntimeOnly(libs.junit.engine)
