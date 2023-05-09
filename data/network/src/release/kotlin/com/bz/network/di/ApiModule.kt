1597f0f61a1e8d873939ed48709267a24a695369
Benedykt Ziobro <benedykt@backbase.com>
2023-05-09 18:48:54 +0200

    fix : add missing release

--- data/network/src/release/kotlin/com/bz/network/di/ApiModule.kt
+++ data/network/src/release/kotlin/com/bz/network/di/ApiModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
 const val BASE_URL = "https://api.themoviedb.org/3/"
 
