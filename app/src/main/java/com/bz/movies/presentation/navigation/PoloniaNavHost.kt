9245e556a19fb58f2b1391618ecb23e1cedad688
Benedykt Ziobro <benedykt@backbase.com>
2023-05-11 08:23:27 +0200

    feature : create bottom tabs

---
+++ app/src/main/java/com/bz/movies/presentation/navigation/PoloniaNavHost.kt
@@ -0,0 +1,37 @@
+package com.bz.movies.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.bz.movies.presentation.screens.empty.Greeting
+
+
+@Composable
+fun MoviesNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = RootRoute.PlayingNow.route,
+    ) {
+        composable(RootRoute.PlayingNow.route) {
+            Greeting(RootRoute.PlayingNow.route)
+        }
+        composable(RootRoute.Popular.route) {
+            Greeting(RootRoute.Popular.route)
+        }
+        composable(RootRoute.Favorite.route) {
+            Greeting(RootRoute.Favorite.route)
+        }
+        composable(RootRoute.More.route) {
+            Greeting(RootRoute.More.route)
+        }
+    }
+}
+
+
+
