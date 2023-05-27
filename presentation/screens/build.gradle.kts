ac882de63885701c12c61697ac6ab2097d44c854
Benedykt Ziobro <benedykt@backbase.com>
2023-05-27 08:13:07 +0200

    fix : move compose opt-in to only screens package

--- presentation/screens/build.gradle.kts
+++ presentation/screens/build.gradle.kts
@@ -31,6 +31,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions {
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+            "-opt-in=androidx.compose.material.ExperimentalMaterialApi",
+        )
+    }
     @Suppress("UnstableApiUsage")
     buildFeatures {
         compose = true
