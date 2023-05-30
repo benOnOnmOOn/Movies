b42743cb2336200ffc7319329c7d5a8735ed3fa9
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2023-05-30 08:14:57 +0200

    Pull to refresh (#45)
    Co-authored-by: Benedykt Ziobro <benedykt@backbase.com>

--- presentation/screens/src/main/kotlin/com/bz/movies/presentation/screens/common/MoviesState.kt
+++ presentation/screens/src/main/kotlin/com/bz/movies/presentation/screens/common/MoviesState.kt
@@ -2,6 +2,7 @@ package com.bz.movies.presentation.screens.common
 
 data class MoviesState(
     val isLoading: Boolean = true,
+    val isRefreshing: Boolean = false,
     val playingNowMovies: List<MovieItem> = emptyList()
 )
 
