67269417c604e18dd3db5384fc7dba3a1af3939b
Benedykt Ziobro <benedykt@backbase.com>
2024-02-22 09:04:33 +0100

    InternetConnectionImplTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.com.google.dagger.hilt.android)
     alias(libs.plugins.org.jetbrains.kotlinx.kover)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.dependency.guard)
 }
 
 android {
@@ -90,4 +91,20 @@ dependencies {
     androidTestImplementation(libs.androidx.monitor)
     androidTestImplementation(libs.junit.api)
     androidTestRuntimeOnly(libs.junit.engine)
+}
+
+configurations {
+    implementation {
+        exclude("com.google.guava", "failureaccess")
+        exclude("com.google.guava", "guava")
+        exclude("com.google.guava", "listenablefuture")
+        exclude("com.google.code.findbugs", "jsr305")
+    }
+}
+
+dependencyGuard {
+    // All dependencies included in Production Release APK
+    configuration("releaseRuntimeClasspath") {
+        tree = true
+    }
 }
