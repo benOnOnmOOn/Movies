4af537d1f4b08459d338b0bea02c275a8625acb7
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-09-30 09:53:14 +0200

    update deps

--- presentation/screens/src/main/kotlin/com/bz/movies/presentation/theme/Theme.kt
+++ presentation/screens/src/main/kotlin/com/bz/movies/presentation/theme/Theme.kt
@@ -51,6 +51,7 @@ fun MoviesTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            @Suppress("DEPRECATION")
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
