b85c9ebd8e177237b6a0844d7cace1c833f24869
Benedykt Ziobro <benedykt.ziobro@gmail.com>
2024-11-11 22:07:02 +0100

    Fixes for navigation

--- presentation/screens/src/test/java/com/bz/movies/presentation/screens/details/MovieDetailsViewModelTest.kt
+++ presentation/screens/src/test/java/com/bz/movies/presentation/screens/details/MovieDetailsViewModelTest.kt
@@ -18,7 +18,9 @@ import io.mockk.verify
 import kotlin.random.Random
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterAll
@@ -59,6 +61,7 @@ class MovieDetailsViewModelTest {
 
         val viewModel = MovieDetailsViewModel(Lazy { movieRepository })
         viewModel.fetchMovieDetails(1234)
+        runCurrent()
         viewModel.state.test {
             val actualItem = awaitItem()
             assertEquals(EXPECTED_DETAILS_STATE, actualItem)
